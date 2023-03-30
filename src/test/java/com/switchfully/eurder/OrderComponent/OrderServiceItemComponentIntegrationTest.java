5d872c111747f4d15d1f6024dd8f3abea5d97d4e
Wouter Sels <phaerocode@hotmail.com>
2023-03-30 09:51:11 +0200

    added jenkins file

--- src/test/java/com/switchfully/eurder/OrderComponent/OrderServiceItemComponentIntegrationTest.java
+++ src/test/java/com/switchfully/eurder/OrderComponent/OrderServiceItemComponentIntegrationTest.java
@@ -1,6 +1,33 @@
 package com.switchfully.eurder.OrderComponent;
 
+import com.switchfully.eurder.ItemComponent.IItemService;
+import com.switchfully.eurder.api.dto.order.CreateOrderDTO;
+import com.switchfully.eurder.api.dto.order.ItemGroupDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class OrderServiceItemComponentIntegrationTest {
     //TODO: check if the ItemService works in OrderComponent
-    //See if it returns a value which we can use
+
+    private IItemService itemService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+    private OrderMapper orderMapper;
+
+    @BeforeEach
+    void setup(){
+        orderRepository = new OrderRepository();
+        orderMapper = new OrderMapper();
+        orderService = new OrderService(orderRepository,orderMapper,itemService);
+        itemService = Mockito.mock(IItemService.class);
+    }
+
+    @Test
+    @DisplayName("Integration test for itemComponent in orderService")
+    void IntegrationTest (){
+        private final ItemGroupDTO();
+        private final CreateOrderDTO();
+    }
 }
